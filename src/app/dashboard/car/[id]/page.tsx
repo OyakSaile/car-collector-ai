917df76d81755bde534c4bfe122ed6f4f302fa24
Kayo Elias <contatodevkayo@gmail.com>
2024-06-15 10:11:16 -0300

    feature: car page

--- src/app/dashboard/car/[id]/page.tsx
+++ src/app/dashboard/car/[id]/page.tsx
@@ -1,9 +1,16 @@
-import { Footer } from "@/components/ui/footer";
+import Image from "next/image";
 
 export default function CarPage() {
   return (
     <main className="h-screen w-full relative">
-      <Footer className="absolute bottom-0" />
+      <div className="relative h-[300px]">
+        <Image objectFit="cover" src="/bmw.jpg" fill alt="car" />
+      </div>
+      <div className="bg-white dark:bg-primary-foreground h-full p-4">
+        <h2 className="text-xl text-black dark:text-white uppercase font-bold">
+          Nissan Skyline R34
+        </h2>
+      </div>
     </main>
   );
 }
